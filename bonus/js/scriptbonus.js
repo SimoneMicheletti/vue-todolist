073b7040c986d02aa3905c9cdb1d3a76a9f5afa3
SimoneMicheletti <sm.micheletti@gmail.com>
2020-11-19 12:09:41 +0100

    Esercizio rifatto con oggetti anzichè array

--- bonus/js/scriptbonus.js
+++ bonus/js/scriptbonus.js
@@ -4,64 +4,58 @@ var app = new Vue({
   data: {
     inputElement: "",
     arrayElementToDo: [],
-    doble: [],
-    classActive: []
   },
 
   methods: {
 
     // Function add element
     addElement: function() {
-      if (this.inputElement != "") {
-        if (this.arrayElementToDo.includes(this.inputElement)) {
-          this.doble.push(true);
-        } else {
-          this.doble.push(false);
+      if (this.inputElement.name != "") {
+
+        let newElement = {};
+        newElement.name = this.inputElement;
+        newElement.doble = false;
+        newElement.classActive = false;
+
+        for (var i = 0; i < this.arrayElementToDo.length; i++) {
+          if (this.arrayElementToDo[i].name === newElement.name ) {
+            newElement.doble = true;
+          }
         }
-        this.arrayElementToDo.push(this.inputElement);
-        this.classActive.push(false);
+
+        this.arrayElementToDo.push(newElement);
         this.inputElement = "";
         this.resetActiveClass();
-        console.log(this.arrayElementToDo);
-        console.log(this.doble);
-        console.log(this.classActive);
-
       }
     },
 
     // Function remove element
     removeElement: function(indexElement) {
-      this.arrayElementToDo.splice(indexElement, 1);
-      this.doble.splice(indexElement, 1);
-      this.classActive.splice(indexElement, 1);
-      Vue.set(this.classActive, indexElement, false);
-      console.log(this.arrayElementToDo);
-      console.log(this.doble);
-      console.log(this.classActive);
+      Vue.delete(this.arrayElementToDo, indexElement)
+      this.resetActiveClass();
     },
 
     // Function to manage the "doble" class
     removeDoble: function(indexElement) {
-      Vue.set(this.doble, indexElement, false);
+      this.arrayElementToDo[indexElement].doble = false
       this.resetActiveClass();
     },
 
     // Function to manage the "active" class
     classActiveOnOff: function(indexElement) {
-      if (this.classActive[indexElement] === false) {
+      if (this.arrayElementToDo[indexElement].classActive === false) {
         this.resetActiveClass();
-        Vue.set(this.classActive, indexElement, true);
+        this.arrayElementToDo[indexElement].classActive = true
       } else {
-        Vue.set(this.classActive, indexElement, false);
+        this.arrayElementToDo[indexElement].classActive = false
       }
     },
 
     // Function to reset "active" class
     resetActiveClass: function() {
-      for (var i = 0; i < this.classActive.length; i++) {
-        Vue.set(this.classActive, i, false);
+      for (var i = 0; i < this.arrayElementToDo.length; i++) {
+        this.arrayElementToDo[i].classActive = false
       }
     }
-
   }
 })
